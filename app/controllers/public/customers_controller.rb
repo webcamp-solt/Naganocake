e49d784681c146172cdd00001493385261a1ddb7
m9795 <106650955+m9795@users.noreply.github.com>
2022-07-19 20:20:18 +0900

    [Add]編集画面,編集機能追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,14 +4,23 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(current_customer.id)
   end
 
   def update
+    @customer = Customer.find(current_customer.id)
+    @customer.update(customer_params)
+    redirect_to customers_my_page_path
   end
 
   def out_check
   end
 
   def out
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :zip_code, :address, :phone_number, :email, :customer_status)
   end
 end
