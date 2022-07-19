52854e1c23c3083749d344517144e838310a242f
m9795 <106650955+m9795@users.noreply.github.com>
2022-07-19 18:41:59 +0900

    [Add]マイページの編集

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -11,4 +11,8 @@ class Customer < ApplicationRecord
   def full_name
     self.last_name + "　" + self.first_name
   end
+
+  def full_name_kana
+    self.last_name_kana + "　" + self.first_name_kana
+  end
 end
