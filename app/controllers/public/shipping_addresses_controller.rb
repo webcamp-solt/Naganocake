f02deb4abf6c6bd2c2843d3268411ed35753f92c
EC2 Default User <ec2-user@ip-172-31-14-43.ec2.internal>
2022-07-19 13:11:51 +0000

    [upload]function

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,16 +1,36 @@
 class Public::ShippingAddressesController < ApplicationController
   def index
-  end
-
-  def edit
+    @shipping_addresses = ShippingAddress.all
+    @shipping_address = ShippingAddress.new
   end
 
   def create
+    @shipping_address = ShippingAddress.new(shipping_address_params)
+    @shipping_address.save
+    redirect_to shipping_addresses_path
   end
 
+
+  def edit
+    @shipping_address = ShippingAddress.find(params[:id])
+  end
+
+
   def update
+    @shipping_address = ShippingAddress.find(params[:id])
+     if @book.update(shipping_address_params)
+      redirect_to shipping_addresses_path, notice: "You have updated user successfully."
+    else
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def shipping_address_params
+    params.require(:shipping_address).permit(:zip_code, :address, :name, :customer_id)
   end
 end
