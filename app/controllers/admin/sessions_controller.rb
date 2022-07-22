62f2386cf0661805b43e807087a3ac78bee128b8
EC2 Default User <ec2-user@ip-172-31-14-43.ec2.internal>
2022-07-22 13:38:30 +0000

    [up]admin logout

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -17,17 +17,13 @@ class Admin::SessionsController < Devise::SessionsController
   # def destroy
   #   super
   # end
-
-    before_action :authenticate_admin!
+  def after_sign_in_path_for(resource)
+      admin_login_path
+  end
 
   def after_sign_out_path_for(resource)
-    new_admin_session_path(resource)
+      new_admin_session_path
   end
-
-  def after_sign_in_path_for(resource)
-    admin_login_path(resource)
-  end
-
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
