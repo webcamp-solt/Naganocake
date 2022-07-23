3d6f23af853a5f49c19289b7ca25a376cae2f474
m9795 <106650955+m9795@users.noreply.github.com>
2022-07-23 17:15:44 +0900

    [Fix]顧客情報編集失敗時のredirect先追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -14,8 +14,11 @@ class Admin::CustomersController < ApplicationController
 
   def update
     @customer = Customer.find(params[:id])
-    @customer.update(customer_params)
-    redirect_to admin_customer_path(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(params[:id])
+    else
+      redirect_to request.referer
+    end
   end
 
   private
