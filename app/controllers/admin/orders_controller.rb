79e152c1a6b383dbbc380ffe8183a0a6b5f5c8fa
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-07-24 05:25:29 +0000

    [Update] 注文履歴

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,20 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
   end
 
   def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      if @order.order_status == "payment_confirmation"
+        @order.ordered_items.update(production_status: "waiting_for_production")
+      end
+    redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:order_status)
   end
 end
