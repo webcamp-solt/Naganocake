25411023e8f5cad7360b67aa93c309086b9e2da4
Narumi Abe <smile.narukkory@gmail.com>
2022-07-26 03:12:46 +0000

    [Add]ロゴ追加

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,5 +1,5 @@
 class Public::ItemsController < ApplicationController
-  before_action :authenticate_customer!, except: [:index]
+  before_action :authenticate_customer!, except: [:index, :show]
 
   def index
     @genres = Genre.all
